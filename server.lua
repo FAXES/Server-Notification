5e6174a9ee9e5560deb4f511ce3017a5c044605a
FAXES <31368070+FAXES@users.noreply.github.com>
2018-07-23 14:53:16 +1000

    1.5

--- server.lua
+++ server.lua
@@ -7,51 +7,51 @@
 rPeacetime = true		-- Enables the peacetime command(s) Values: true, false
 
 -- Code --
-AddEventHandler('chatMessage', function(player, playerName, message)
+AddEventHandler('chatMessage', function(source, name, msg)
 
 -- Server Restart Commands List Command
 RegisterCommand("svrlist", function()
-	if IsPlayerAceAllowed(player, "fax.cmds") then
-		TriggerClientEvent('RestartList', player)
+	if IsPlayerAceAllowed(source, "fax.cmds") then
+		TriggerClientEvent('RestartList', source)
 	else
-		TriggerClientEvent('Restart:NoPerms', player)
+		TriggerClientEvent('Restart:NoPerms', source)
 	end
 end)
 
 -- Server Restart in 5 Mins Command
 RegisterCommand("svr5", function()
-	if IsPlayerAceAllowed(player, "fax.cmds") then
+	if IsPlayerAceAllowed(source, "fax.cmds") then
 		TriggerClientEvent("chatMessage", -1, " \n —————————————————————— \n SERVER RESTART IN 5 MINUTES \n Please Finish Your Current RP Scenario \n ——————————————————————", {239, 0, 0})
 	else
-		TriggerClientEvent('Restart:NoPerms', player)
+		TriggerClientEvent('Restart:NoPerms', source)
 	end
 end)
 
 -- Server Restart in 10 Mins Command
 RegisterCommand("svr10", function()
-	if IsPlayerAceAllowed(player, "fax.cmds") then
+	if IsPlayerAceAllowed(source, "fax.cmds") then
 		TriggerClientEvent("chatMessage", -1, " \n —————————————————————— \n SERVER RESTART IN 10 MINUTES \n Please Finish Your Current RP Scenario \n ——————————————————————", {239, 0, 0})
 	else
-		TriggerClientEvent('Restart:NoPerms', player)
+		TriggerClientEvent('Restart:NoPerms', source)
 	end
 end)
 
 -- Server Restart Now Command
 RegisterCommand("svrnow", function()
-	if IsPlayerAceAllowed(player, "fax.cmds") then
+	if IsPlayerAceAllowed(source, "fax.cmds") then
 		TriggerClientEvent("chatMessage", -1, " \n —————————————————————— \n SERVER RESTART IS IN PROGRESS \n Please Rejoin the server  \n ——————————————————————", {239, 0, 0})
 		SetGameType("SERVER CURRENTLY RESTARTING")
 			AddEventHandler("playerConnecting", function()
 				DropPlayer(source, 'The Server is restarting, Rejoin in a minute.')
 			end)
 	else
-		TriggerClientEvent('Restart:NoPerms', player)
+		TriggerClientEvent('Restart:NoPerms', source)
 	end
 end)
 
 -- Server Restart Start Timer
 RegisterCommand("svrstart", function()
-	if IsPlayerAceAllowed(player, "fax.cmds") then
+	if IsPlayerAceAllowed(source, "fax.cmds") then
 		TriggerClientEvent("chatMessage", -1, " \n —————————————————————— \n SERVER RESTART IN 10 MINUTES \n Please Finish Your Current RP Scenario \n ——————————————————————", {239, 0, 0})
 		Wait(300000)
 		TriggerClientEvent("chatMessage", -1, " \n —————————————————————— \n SERVER RESTART IN 5 MINUTES \n Please Finish Your Current RP Scenario \n ——————————————————————", {239, 0, 0})
@@ -60,27 +60,27 @@ RegisterCommand("svrstart", function()
 		TriggerClientEvent("chatMessage", -1, " \n —————————————————————— \n SERVER RESTART IS IN PROGRESS \n Please Rejoin the server  \n ——————————————————————", {239, 0, 0})
 		SetGameType("SERVER CURRENTLY RESTARTING")
 	else
-		TriggerClientEvent('Restart:NoPerms', player)
+		TriggerClientEvent('Restart:NoPerms', source)
 	end
 end)
 
 -- Command for Peace Time on
 RegisterCommand("pton", function()
-	if IsPlayerAceAllowed(player, "fax.cmds") then
+	if IsPlayerAceAllowed(source, "fax.cmds") then
 		TriggerClientEvent("chatMessage", -1, " \n —————————————————————— \n PEACE TIME IS NOW IN EFFECT \n This Means No Priority Calls.  \n ——————————————————————", {239, 0, 0})
 		SetGameType("Peace Time In Effect")
 	else
-		TriggerClientEvent('Restart:NoPerms', player)
+		TriggerClientEvent('Restart:NoPerms', source)
 	end
 end)
 
 -- Command for Peace Time on
 RegisterCommand("ptoff", function()
-	if IsPlayerAceAllowed(player, "fax.cmds") then
+	if IsPlayerAceAllowed(source, "fax.cmds") then
 		TriggerClientEvent("chatMessage", -1, " \n —————————————————————— \n PEACE TIME IS NO LONGER IN EFFECT \n Resume Normal RP.  \n ——————————————————————", {239, 0, 0})
 			SetGameType("Freeroam")
 	else
-		TriggerClientEvent('Restart:NoPerms', player)
+		TriggerClientEvent('Restart:NoPerms', source)
 	end
 end)
 
